9e1acbf0896a9b8679d15fb7957ff02cc6f8fbb4
AlleywayNate <nathenwilliams600@gmail.com>
2023-05-11 13:33:04 -0500

    redo plz work

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search) ? "True" : "False");
+        if (aStack.Contains(search))
+        {
+            while (aStack.Pop() != search)
+                continue;
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
